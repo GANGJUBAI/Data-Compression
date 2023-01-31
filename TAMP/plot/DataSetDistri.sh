df81aa31719f4028bdbd286c2190e82a9fda0e96
GANGJUBAI <63623047+GANGJUBAI@users.noreply.github.com>
2023-01-31 16:51:23 +0800

    Delete TAMP directory

--- TAMP/plot/DataSetDistri.sh
+++
@@ -1,3 +0,0 @@
-#List="density diffusivity pressure velocityz"
-sh Compress_TAMP.sh >> fuzzyMedian_Distri_Heat3d.result
-
--- TAMP/plot/parallel_Expandability.sh
+++
@@ -1,8 +0,0 @@
-sh Compress_TAMP_pthreads.sh >> ./Timeresult/overhead_pTAMP128.result
-sh Compress_MedianOdd_Pthreads.sh >> ./Timeresult/overhead_pmedianOdd128.result
-sh Compress_pFPC.sh >> ./Timeresult/overhead_pfpc128.result
-
-
-python3 find.py ./Timeresult/overhead_pTAMP128.result 
-python3 find.py ./Timeresult/overhead_pmedianOdd128.result
-python3 find.py ./Timeresult/overhead_pfpc128.result
